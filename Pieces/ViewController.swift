599ebc1335d47179232e75b21067579f44959b8f
Goodnews <krgoodnews@gmail.com>
2017-11-18 09:52:27 +0900

    Init Project / Default Setting for only code

---
+++ Pieces/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pieces
+//
+//  Created by Goodnews on 2017. 11. 18..
+//  Copyright © 2017년 goodnews. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
