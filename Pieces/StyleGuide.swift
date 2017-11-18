ee92e521de0f4014f7d3157ddd244c40a7f8ed63
Goodnews <krgoodnews@gmail.com>
2017-11-18 10:08:44 +0900

    StyleGuide

---
+++ Pieces/StyleGuide.swift
@@ -0,0 +1,34 @@
+//
+//  StyleGuide.swift
+//  Pieces
+//
+//  Created by Goodnews on 2017. 11. 18..
+//  Copyright © 2017년 goodnews. All rights reserved.
+//
+
+import UIKit
+
+class CustomNavigationController: UINavigationController {
+	override var preferredStatusBarStyle: UIStatusBarStyle {
+		return .lightContent
+	}
+}
+extension UIColor {
+	
+//	static let pWhite = UIColor("FBFCFD")
+//	static let pBlue = UIColor("7BABED")
+//	static let pPink = UIColor("F5C1D7")
+//	static let pYellow = UIColor("FBEBBE")
+//	static let pGray = UIColor("CED6E2")
+
+	static let gold = UIColor("CDA34F")
+	static let daisy = UIColor("E9E7DA")
+	static let stem = UIColor("636B46")
+	static let greenery = UIColor("373F27")
+	
+}
+
+extension UIFont {
+	
+}
+
